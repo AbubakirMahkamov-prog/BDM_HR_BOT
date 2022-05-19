36ca5b38789e9336ae3c9516a2de7ec8f959aaf4
Abubakir <MahkamovAbubakir65119@gmail.com>
2022-05-19 11:41:18 +0500

    Creating project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <br><br>
+    <div>
+    <b-button-group style="width:350px;display: flex;justify-content: space-between;margin:auto">
+      <b-button variant="info" style="width:150px;color: white;" @click="$router.push('newemployee')">👨‍🎓👩‍🎓 Shogirt tushish</b-button>
+      <b-button variant="info" style="width:150px;color: white;">📩 Arizalarim</b-button>
+    </b-button-group>
+  </div>
+  <br>
+  <div>
+    <b-button-group style="width:350px;">
+      <b-button variant="info" style="color:white">🏬 Biz haqimizda</b-button>
+      <b-button variant="info" style="color:white">🏢 Bizning manzil</b-button>
+    </b-button-group>
+  </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
---
+++ src/views/NewEmployee.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="NewEmployee">
+<h1>Salom</h1>
+  </div>
+</template>
